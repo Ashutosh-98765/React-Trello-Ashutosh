2e2b00787330468fc22d41de3fd86ffa9ba2b346
Ashutosh-98765 <asshu65656@gmail.com>
2024-11-04 19:59:37 +0530

    Add Navbar.jsx file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@emotion/react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrello } from "@fortawesome/free-brands-svg-icons/faTrello";
+
+function Navbar() {
+  const theme = useTheme();
+  return (
+    <AppBar position="static" sx={{ bgcolor: theme.palette.primary.main }}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 1,
+            fontFamily: theme.typography.fontFamily,
+          }}
+        >
+          <FontAwesomeIcon icon={faTrello} />
+          TRELLO
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
